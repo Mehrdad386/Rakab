353437bcdf3ceaf373db897b8526c0bcbefe9b30
Mehrdad <mehrdadmrs10@gmail.com>
2024-05-28 15:42:02 +0330

    all files bugs fixed

--- Entity/Declration/Player.cpp
+++ Entity/Declration/Player.cpp
@@ -6,6 +6,7 @@ const std::string Player::getName(){ return name ; }
 const int Player::getAge(){ return age ; }
 const std::string Player::getColor(){ return color ; }
 const bool Player::getIsPassed(){ return isPassed ; }
+const int Player::getCanWar() { return CanWar; } // add by SIna
 
 
 
@@ -14,6 +15,7 @@ void Player::setName( std::string name ){ this->name = name ; }
 void Player::setAge( int age ){ this->age = (age>=0)?age:0 ; }
 void Player::setColor ( std::string color ){ this->color = color ; }
 void Player::setIsPasssed( bool isPassed ){ this->isPassed = isPassed ; }
+void Player::setCanWar(int canwar) { CanWar = canwar; } // add by Sina
 
 
 //constructors
--- Entity/Interface/Game.h
+++ Entity/Interface/Game.h
@@ -1,5 +1,5 @@
-#ifndef GAME
-#define GAME
+#ifndef GAME_H
+#define GAME_H
 #include"Manager.h"
 #include"Map.h"
 #include"Player.h"
