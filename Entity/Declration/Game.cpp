60dc17548d21c674d9b1ffc9a79615023e5566b7
Mehrdad <mehrdadmrs10@gmail.com>
2024-05-29 17:01:22 +0330

    print method of game complited

--- Entity/Declration/Game.cpp
+++ Entity/Declration/Game.cpp
@@ -1,162 +1,185 @@
-#include<iostream>
-#include<stdlib.h>
-#include"../Interface/Game.h"
-#include<time.h>
+#include <iostream>
+#include <stdlib.h>
+#include "../Interface/Game.h"
+#include <time.h>
 
-
-
-Game::Game(){
-    std::srand(time(0)) ;
+Game::Game()
+{
+    std::srand(time(0));
 }
 
-void Game::takeGameInfo(){
-    std::cout<<"enter the number of players: " ;
-    int playerNumber{} ;
+void Game::takeGameInfo()
+{
+    std::cout << "enter the number of players: ";
+    int playerNumber{};
 
-    while (true){
-        std::cin>>playerNumber ;
-        if(playerNumber<=6 && playerNumber >=3)
+    while (true)
+    {
+        std::cin >> playerNumber;
+        if (playerNumber <= 6 && playerNumber >= 3)
             break;
     }
 
-    players.resize(playerNumber) ;
-    std::string name ;
-    int age ;
-    std::string color ;
+    players.resize(playerNumber);
+    playedCards.resize(playerNumber);
+    std::string name;
+    int age;
+    std::string color;
 
-    for(int i{} ; i<playerNumber ; i++){
-        std::cout<<"enter player"<<i+1<<" name: " ;
-        std::cin>>name ;
-        do{
-        std::cout<<"enter player"<<i+1<<" age: " ;
-        std::cin>>age ;
-        }while ( age <0 ) ;
-        do{
-        std::cout<<"choose player"<<i+1<<"'s color(red,blue,yellow,green,purple,gray): " ;
-        std::cin>>color ;
-        for(int j = i-1 ; j>=0 ; j--){
-            if(color == players[j].getColor())
-                color = "none" ;
-        }
-        }while(color != "red" && color != "blue" && color != "yellow" && color != "green" && color != "gray" && color != "purple" ) ;
-        players[i].setName(name) ;
-        players[i].setAge(age) ;
-        players[i].setColor(color) ;
-        
+    for (int i{}; i < playerNumber; i++)
+    {
+        std::cout << "enter player" << i + 1 << " name: ";
+        std::cin >> name;
+        do
+        {
+            std::cout << "enter player" << i + 1 << " age: ";
+            std::cin >> age;
+        } while (age < 0);
+        do
+        {
+            std::cout << "choose player" << i + 1 << "'s color(red,blue,yellow,green,purple,gray): ";
+            std::cin >> color;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (color == players[j].getColor())
+                    color = "none";
+            }
+        } while (color != "red" && color != "blue" && color != "yellow" && color != "green" && color != "gray" && color != "purple");
+        players[i].setName(name);
+        players[i].setAge(age);
+        players[i].setColor(color);
+        playedCards[i].person = players[i];
     }
-    
 }
 
+void Game::print()
+{
+    system("CLS");
+    // first part
+    std::cout << "------------------------------------------------------\n";
+    for (int i{}; i < players.size(); i++)
+    {
+        std::cout << playedCards[i].person.getName() << ": ";
 
+        for (int j{}; j < playedCards[i].cards.size(); j++)
+        {
+            std::cout << playedCards[i].cards[j].getName();
+        }
 
-void Game::print(){
-
-    //second part
-    std::cout<<"------------------------------------------------------" ;
-    for(int i{} ; i<players.size() ; i++){
-        players[i].printCities() ;
-        std::cout<<std::endl ;
+        std::cout << std::endl;
     }
-    std::cout<<"------------------------------------------------------" ;
-    //third part
-    input() ;
-    std::cout<<"------------------------------------------------------" ;
 
+    // second part
+    std::cout << "------------------------------------------------------\n";
+    for (int i{}; i < players.size(); i++)
+    {
+        players[i].printCities();
+        std::cout << std::endl;
+    }
+    std::cout << "------------------------------------------------------\n";
+    // third part
+    input();
+    std::cout << "------------------------------------------------------\n";
 }
 
+void Game::input()
+{
 
-void Game::input(){
+    if (!players[turn].getIsPassed())
+    {
+        std::cout << "choose a card to play or pass(use camel case): ";
+        std::string choice;
+        std::cin >> choice;
 
-    if(!players[turn].getIsPassed()){
-        std::cout<<"choose a card to play or pass(use camel case): " ;
-        std::string choice ;
-        std::cin>>choice ;
-
-        if(choice == "pass"){
-            players[turn].setIsPasssed(true) ;
-            turn++ ;
+        if (choice == "pass")
+        {
+            players[turn].setIsPasssed(true);
+            turn++;
         }
-        else{
-            playedCards[turn].cards.push_back(players[turn].play(choice)) ;    
-            turn++ ;
+        else
+        {
+            playedCards[turn].cards.push_back(players[turn].play(choice));
+            turn++;
         }
     }
-    else{
-        turn++ ;
+    else
+    {
+        turn++;
     }
-
 }
 
-//to fill the players hand based on the number of the token cities by them
-void Game::fillCards(){
+// to fill the players hand based on the number of the token cities by them
+void Game::fillCards()
+{
 
-    for(int i{} ; i< players.size() ; i++){
-        for(int j{} ; j< 10 + players[i].getNumberOfCities() ; j++){
-            int random = rand()%cards.size() ;
-            players[i].addCard(cards[random]) ;
-            cards.erase(cards.begin() + random - 1) ;
+    for (int i{}; i < players.size(); i++)
+    {
+        for (int j{}; j < 10 + players[i].getNumberOfCities(); j++)
+        {
+            int random = rand() % cards.size();
+            players[i].addCard(cards[random]);
+            cards.erase(cards.begin() + random - 1);
         }
     }
-
 }
 
-//to make cards vector based on game info
-void Game::generateCards(){
-    
-    YellowCard y1 (1 , "1") ;
-    YellowCard y2 (2 , "2") ;
-    YellowCard y3 (3 , "3") ;
-    YellowCard y4 (4 , "4") ;
-    YellowCard y5 (5 , "5") ;
-    YellowCard y6 (6 , "6" ) ;
-    YellowCard y10 (10 , "10") ;
-    Bahar bahar ;
+// to make cards vector based on game info
+void Game::generateCards()
+{
+
+    YellowCard y1(1, "1");
+    YellowCard y2(2, "2");
+    YellowCard y3(3, "3");
+    YellowCard y4(4, "4");
+    YellowCard y5(5, "5");
+    YellowCard y6(6, "6");
+    YellowCard y10(10, "10");
+    Bahar bahar;
     Zemestan zemestan;
-    ShahDokht shahdokht ;
-    TablZan tablzan ;
-    Matarsak matarsak ;
-    
-    //pushing yellow cards
-    for(int i {} ; i<10 ; i++){
-        cards.push_back(y1) ;
-        if(i<8)
-            cards.push_back(y2) ;
-            cards.push_back(y3) ;
-            cards.push_back(y4) ;
-            cards.push_back(y5) ;
-            cards.push_back(y6) ;
-            cards.push_back(y10) ;
+    ShahDokht shahdokht;
+    TablZan tablzan;
+    Matarsak matarsak;
+
+    // pushing yellow cards
+    for (int i{}; i < 10; i++)
+    {
+        cards.push_back(y1);
+        if (i < 8)
+            cards.push_back(y2);
+        cards.push_back(y3);
+        cards.push_back(y4);
+        cards.push_back(y5);
+        cards.push_back(y6);
+        cards.push_back(y10);
     }
 
-    for(int i{} ; i<16 ; i++){
+    for (int i{}; i < 16; i++)
+    {
 
-        cards.push_back(matarsak) ;
+        cards.push_back(matarsak);
 
-        if(i<6){
-            cards.push_back(tablzan) ;
+        if (i < 6)
+        {
+            cards.push_back(tablzan);
         }
 
-        if(i<3){
-            cards.push_back(bahar) ;
-            cards.push_back(zemestan) ;
-            cards.push_back(shahdokht) ;
+        if (i < 3)
+        {
+            cards.push_back(bahar);
+            cards.push_back(zemestan);
+            cards.push_back(shahdokht);
         }
-
     }
-
-
 }
 
-
-void Game::setWar(){
-
+void Game::setWar()
+{
 }
 
-void Game::endWar(){
-
+void Game::endWar()
+{
 }
 
-
-void Game::gameFlow(){
-
+void Game::gameFlow()
+{
 }
--- Test.cpp
+++ Test.cpp
@@ -5,6 +5,8 @@
 #include "Entity/Interface/Zemestan.h"
 #include "Entity/Interface/TablZan.h"
 #include "Entity/Interface/Matarsak.h"
+#include "Entity/Interface/Game.h"
+
 
 #include <iostream>
 #include <vector>
@@ -12,38 +14,41 @@ using namespace std;
 void showVec(PlayedCard);
 int main()
 {
-    Card c1{1, "Sina"};
-    Card c2{4, "mehrdad"};
-    Card c3{0, "danial"};
-    Card c4{0, "fazel"};
-    Card c5{5, "5"};
-    PlayedCard pc;
-    pc.cards.push_back(c1);
-    pc.cards.push_back(c2);
-    pc.cards.push_back(c3);
-    pc.cards.push_back(c4);
-    Player P;
-    ShahDokht SD;
-    Bahar B;
-    Zemestan Z;
-    TablZan T;
-    Matarsak M;
-    P.setCanWar(5);
-    P = SD.ability(P);
-    cout << "can War" << P.getCanWar() << endl;
-    pc = B.ability(pc);
-    cout << "after bahar\n";
-    showVec(pc);
-    pc = Z.ability(pc);
-    cout << "after zemestan\n";
-    showVec(pc);
-    pc.cards.push_back(c5);
-    pc = T.ability(pc);
-    cout << "after tablzan\n";
-    showVec(pc);
-    pc = M.ability(pc);
-    cout << "after matarsak\n";
-    showVec(pc);
+    // Card c1{1, "Sina"};
+    // Card c2{4, "mehrdad"};
+    // Card c3{0, "danial"};
+    // Card c4{0, "fazel"};
+    // Card c5{5, "5"};
+    // PlayedCard pc;
+    // pc.cards.push_back(c1);
+    // pc.cards.push_back(c2);
+    // pc.cards.push_back(c3);
+    // pc.cards.push_back(c4);
+    // Player P;
+    // ShahDokht SD;
+    // Bahar B;
+    // Zemestan Z;
+    // TablZan T;
+    // Matarsak M;
+    // P.setCanWar(5);
+    // P = SD.ability(P);
+    // cout << "can War" << P.getCanWar() << endl;
+    // pc = B.ability(pc);
+    // cout << "after bahar\n";
+    // showVec(pc);
+    // pc = Z.ability(pc);
+    // cout << "after zemestan\n";
+    // showVec(pc);
+    // pc.cards.push_back(c5);
+    // pc = T.ability(pc);
+    // cout << "after tablzan\n";
+    // showVec(pc);
+    // pc = M.ability(pc);
+    // cout << "after matarsak\n";
+    // showVec(pc);
+    Game game ;
+    game.print() ;
+
     return 0;
 }
 void showVec(PlayedCard pc)
