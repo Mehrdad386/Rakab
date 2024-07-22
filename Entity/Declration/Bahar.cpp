7cdad0a769e7bacb0d3ffc407e10cdeb336a5a4f
Mehrdad <mehrdadmrs10@gmail.com>
2024-07-22 13:36:49 +0330

    Bahar ability has effect on ShirDokht now

--- Entity/Declration/Bahar.cpp
+++ Entity/Declration/Bahar.cpp
@@ -18,7 +18,7 @@ void Bahar::ability(std::vector<PlayedCard> &pc)
         {
             if (i < pc.size() && j < pc.at(i).cards.size())
             {
-                if (pc.at(i).cards.at(j).getPower() == bigNumber && isYellow(pc.at(i).cards.at(j).getName()))
+                if ((pc.at(i).cards.at(j).getPower() == bigNumber) && ((isYellow(pc.at(i).cards.at(j).getName())) || (pc.at(i).cards.at(j).getName() == "ShirDokht")))
                 {
                     pc.at(i).cards.at(j).setPower(((bigNumber + 3)));
                 }
@@ -27,3 +27,25 @@ void Bahar::ability(std::vector<PlayedCard> &pc)
     }
 }
 
+int Bahar::findBigest(std::vector<PlayedCard> &pc)
+{
+    int bigNumber{0};
+    for (int i{}; i < pc.size(); i++)
+    {
+        for (int j{}; j < pc.at(i).cards.size(); j++)
+        {
+            if (i < pc.size() && j < pc.at(i).cards.size())
+            {
+                if ( (isYellow(pc.at(i).cards.at(j).getName())) || (pc.at(i).cards.at(j).getName() == "ShirDokht") )
+                {
+                    if (pc.at(i).cards.at(j).getPower() > bigNumber)
+                    {
+                        bigNumber = pc.at(i).cards.at(j).getPower();
+                    }
+                }
+            }
+        }
+    }
+
+    return bigNumber;
+}
