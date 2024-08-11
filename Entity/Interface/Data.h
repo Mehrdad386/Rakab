9b44eef510f025d85444097ff9591e661366f66c
Mehrdad <mehrdadmrs10@gmail.com>
2024-08-11 15:47:27 +0330

    little fix in data class

--- Entity/Interface/Data.h
+++ Entity/Interface/Data.h
@@ -6,17 +6,7 @@
 #include "YellowCard.h"
 #include "PirpleCard.h"
 #include "City.h"
-#include "Bahar.h"
-#include "Zemestan.h"
-#include "TablZan.h"
-#include "ShirDokht.h"
-#include "ShirZan.h"
-#include "Matarsak.h"
-#include"RishSefid.h"
-#include"ParchamDar.h"
-#include"FokSefid.h"
-#include"RakhshSefid.h"
-#include"ShirinAghl.h"
+
 
 struct GameData
 {
